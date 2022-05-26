6dce0cbf13097bdedf47055eea77c09b7411ab9f
Ricardo Bonilla Morales <ricardobonillamorales@Ricardos-MacBook-Pro-2.local>
2022-05-25 19:19:57 -0600

    Create cluster, deployment, and service templates for EKS

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,5 +1,8 @@
+# Install eksctl
 ansible-playbook ./ansible/main.yml
 
-eksctl create cluster --name=MLMicroservice --region=us-east-1 --nodes=1 --zones us-east-1a,us-east-1b,us-east-1c,us-east-1d,us-east-1f
+# Create cluster
+eksctl create cluster -f cluster.yml
 
+# List cluster
 eksctl get cluster --name=MLMicroservice --region=us-east-1
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-dockerpath=richbm10/my-private-repo
-
 # Step 2
-# Run the Docker Hub container with kubernetes
-kubectl create deploy ml-microservice --image=$dockerpath:ml-microservice
-
+# Run the ECR container image in EKS
+kubectl apply -f deployment.yml
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-kubectl port-forward pod/ml-microservice-7c57bbd5ff-zvmpq --address 0.0.0.0 3000:80
+# Run kubernetes service so the microservice can be reachable
+kubectl apply -f service.yml
